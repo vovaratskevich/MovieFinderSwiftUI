bb68d7459c31b4a71ee6c936a6d332ec83c5bde9
Vladimir Ratskevich <Vladimir Ratskevich>
2021-03-15 22:43:33 -0700

    Initial Commit

---
+++ MovieFinderSwiftUI/MovieFinderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderSwiftUIApp.swift
+//  MovieFinderSwiftUI
+//
+//  Created by user on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
